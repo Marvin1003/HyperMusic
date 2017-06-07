19f1264b7f93e278af33242b90cf2a0a9dbcaacd
Marvin1003 <marvin.klaproth@gmail.com>
2017-06-07 19:29:26 +0200

    audio control partically

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,67 @@
+
+
+/* INITIALIZE VOLUME */
 window.onload = function()
 {
-  var test = document.getElementById("logo1");
-  function changeimage()
+  var audio = document.getElementById("audio");
+  audio.volume = 0.1;
   {
-    var element = document.getElementById("logo1");
-    element.style.opacity = "1";
-
+    setTimeout(
+      function()
+      {
+      var element = document.getElementById("logo1");
+      element.style.opacity = "1";
+    }, 3000);
   }
-  setTimeout(changeimage, 3000);
 }
 
-/* AUDIO PLAYER */
-function play()
+function playpause()
 {
-  var track = new Audio ("music/TheOcean.mp3");
-  var icon = document.getElementById("playpause");
-  if(icon.textContent = "play_arrow")
+  var icon = document.getElementById("playpausebutton");
+  var audio = document.getElementById("audio");
+  if(icon.textContent === "play_arrow")
+  {
     icon.textContent ="pause";
+    audio.play();
+  }
   else
+  {
     icon.textContent = "play_arrow";
+    audio.pause();
+  }
+}
+/* AUDIO PLAYER */
+/* GLOBAL OBJECTS*/
+var button, slider;
+var clickcount = 0;
+
+window.onclick = function(mouse)
+{
+  button = document.getElementById("volumebutton");
+  slider = document.getElementById("volumeslider");
+  if(clickcount === 2 && (slider.style.display === "block") && (button.style.display === "none")  && (mouse.target.id !== "volumeslider"))
+  {
+    button.style.display = "";
+    slider.style.display = "none";
+  }
+  clickcount = 1;
+  clickcount++;
+}
+function showvolumeslider()
+{
+  clickcount = 0;
+  button = document.getElementById("volumebutton");
+  slider = document.getElementById("volumeslider");
+  if(button.style.display === "")
+  {
+    button.style.display = "none";
+    slider.style.display = "block";
+    clickcount++;
+  }
+}
+
+function setvolume(volume)
+{
+  var audio = document.getElementById("audio");
+  audio.volume = volume/100;
 }
